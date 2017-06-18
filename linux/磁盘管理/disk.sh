965eadaa08ac0b40224b3180df2ef42fcdfa000b
yjbq125 <79076431@qq.com>
2017-06-19 00:24:33 +0800

    文件系统

--- linux/磁盘管理/disk.sh
+++ linux/磁盘管理/disk.sh
@@ -244,8 +244,27 @@ parted命令
 		# parted /dev/vda mkpart primary fat32 36.0G 37.0G	(新建一个1G的分区槽)
 
 
+Btrfs 文件系统 (B-tree,Butter FS,Better FS),GPL,Oracle,2007,CoW;
 
+    核心特性：
+        多物理卷支持：btrfs可由多个底层物理卷组成：支持RAID，以联机“添加”、“移除”，“修改”
+        写时复制更新机制(CoW)：复制、更新及替换指针，而非“就地”更新；
+        数据及元数据校验码：checksum
+        子卷：sub_volume
+        快照：支持快照的快照；能实现增量快照
+        透明压缩：
 
-
-
-
+    文件系统创建：
+        mkfs.btrfs
+            -L 'LABEL'
+            -d <type>：raid0,raid1,raid5,raid6,raid10,single
+            -m <profile>：raid0,raid1,raid5,raid6,raid10,single,dup 
+            -O <feature>
+                -O list-all：列出支持的所有feature;
+        属性查看：
+            btrfs filesystem show
+        挂载文件系统：
+            mount -t btrfs /dev/sdb MOUNT_POINT
+        透明压缩机制：
+            mount -o compress={lzo|zlib} DEVICE MOUNT_POINT
+                
